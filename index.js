ea0c3c0b3df7e1c89af7c1ce1d56b6d9bf228a6a
yusuf-achmad <emergingyusuf@gmail.com>
2023-02-08 10:57:39 +0700

    merapikan folder

---
+++ index.js
@@ -0,0 +1,37 @@
+//luas segitiga
+const alas= 4
+const tinggi= 3
+const luassegitiga= 0.5 *alas * tinggi;
+console.log(luassegitiga)
+
+//luas persegi panjang
+const panjang= 5
+const lebar= 2
+const luaspersegipanjang= panjang * lebar;
+console.log(luaspersegipanjang)
+
+//keliling lingkaran
+const diameter= 8
+const phi= 3.14
+const kelilinglingkaran= diameter * phi;
+console.log(kelilinglingkaran)
+
+//luas lingkaran
+const jarijari= 4
+const luaslingkaran= phi  *jarijari *jarijari
+console.log(luaslingkaran)
+
+//keliling balok
+const panjangbalok=6
+const lebarbalok=4
+const tinggibalok=5
+const kelilingbalok= 4 *(panjangbalok* lebarbalok* tinggibalok);
+console.log(kelilingbalok)
+
+//volume balok
+const volumebalok= panjangbalok*lebarbalok*tinggibalok;
+console.log(volumebalok)
+
+
+
+
---
+++ naya-kursus/04-01/Latihan/main.js
@@ -0,0 +1,25 @@
+let nilai=0;
+if (nilai >=80){
+    console.log('A')
+}
+else if (nilai>=60 && nilai<80){
+    console.log('b');
+}
+else if(nilai>=40 && nilai<60){
+    console.log('c')
+}
+else if(nilai>=1 && nilai <40 ){
+    console.log('d')
+}
+else {console.log('e')}
+
+let jarijari=4
+let luaslingkaran1= 3.14 * jarijari**2
+
+let jarjar=4
+let luaslingkaran2= 3.14 * jarjar**2
+
+if (luaslingkaran1 == luaslingkaran2){
+    console.log("sama")
+}
+else {"tidak sama"}
